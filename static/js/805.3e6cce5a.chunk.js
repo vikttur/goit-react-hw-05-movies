9c202c701b34a33b6daaf8e86332a9f22147b869
vikttur <105513387+vikttur@users.noreply.github.com>
2023-07-14 10:44:38 +0000

    Deploying to gh-pages from @ vikttur/goit-react-hw-05-movies@c391598a820391a2661e6de186d492c3eac544e9 🚀

---
+++ static/js/805.3e6cce5a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(87),h=n(184);t.default=function(){return(0,h.jsxs)("section",{children:[(0,h.jsx)("h1",{children:"Ups...!"}),(0,h.jsx)("h2",{children:"Page not found"}),(0,h.jsx)("h3",{children:(0,h.jsx)(r.rU,{to:"/",children:"- Go to home page -"})})]})}}}]);
+//# sourceMappingURL=805.3e6cce5a.chunk.js.map
