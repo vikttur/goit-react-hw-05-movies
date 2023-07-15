c237ab5a86ae1fc5e32c8559ee0a12030106f886
Viktor Biruk <vikttur.job@gmail.com>
2023-07-15 12:23:30 +0300

    adds a replace attribute to Navigate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" replace="true" />} />
       </Route>
     </Routes>
   );
